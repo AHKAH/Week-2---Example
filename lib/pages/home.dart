7a473a8f3108c732aad052be2417cb46403fc0e5
Arman Hamzehlou <hamzehlou1369@gmail.com>
2023-11-13 00:01:51 -0500

    Column

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: sized_box_for_whitespace
+
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatelessWidget {
@@ -9,14 +11,19 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Simple Flutter Tree'),
       ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Hello, Flutter!',
-            )
-          ],
+      body: SafeArea(
+        child: Container(
+          // Make it fill the width of the app
+          width: double.infinity,
+          //try remove the line above and see what happens.
+          child: const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('Welcome to my first app!'),
+              Text('My second text')
+            ],
+          ),
         ),
       ),
     );
