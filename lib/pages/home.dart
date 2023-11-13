d049661f2fffc956c3c369fe9fd0d27ca19e813a
Arman Hamzehlou <hamzehlou1369@gmail.com>
2023-11-13 04:09:34 -0500

    Buttons

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,37 +1,35 @@
 import 'package:flutter/material.dart';
+
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Common Buttons Example'),
+        title: const Text('Buttons Example'),
       ),
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            FloatingActionButton(
-              onPressed: () {}, 
-              child: const Icon(Icons.add),
-            ),
-            const SizedBox(height: 8), 
-            ElevatedButton.icon(
-              onPressed: () {}, 
-              icon: const Icon(Icons.navigation),
-              label: const Text('Navigate'),
-            ),
-            const SizedBox(height: 8),
-            IconButton(
-              icon: const Icon(Icons.volume_up),
-              onPressed: () {}, 
-            ),
-            const SizedBox(height: 8), 
-            OutlinedButton(
-              onPressed: () {},
-              child: const Text('Outline'),
-            ),
-          ],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FloatingActionButton(
+                onPressed: () {/*Do something here*/},
+                child: const Icon(Icons.add),
+              ),
+              const SizedBox(width: 16, height: 20),
+              FloatingActionButton.extended(
+                onPressed: () {/*Do something here*/},
+                label: const Text('Add'),
+                icon: const Icon(Icons.add),
+              ),
+              const SizedBox(width: 16, height: 20),
+              IconButton(
+                  iconSize: 60,
+                  icon: const Icon(Icons.access_time),
+                  onPressed: () {/*Do something here*/})
+            ],
+          ),
         ),
       ),
     );
